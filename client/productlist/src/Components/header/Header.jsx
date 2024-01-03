cecd66c6df868804409211f5334c40046e4970ac
Vishnu <vishnu_ot@yahoo.com>
2024-01-03 14:46:33 +0530

    initial commit

---
+++ client/productlist/src/Components/header/Header.jsx
@@ -0,0 +1,16 @@
+import { AppBar, Box, Toolbar } from "@mui/material";
+import React from "react";
+
+function Header() {
+  return (
+    <Box>
+      <AppBar>
+        <Toolbar>
+            
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Header;
